83134427c2d6f689209beb79c9aba24cee1f1fb3
Dania Hih <73383276+daniahih@users.noreply.github.com>
2025-04-07 20:05:54 +0300

    added theme

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import numpy as np
 st.title('Streamlit Example')
 st.subheader('This is a subheader') 
 
+st.markdown("[link to my portfolio ](https://github.com/daniahih/stream_lit-)")
 st.subheader('Line Chart')
 chart_data = pd.DataFrame(
     np.random.randn(20, 3),
